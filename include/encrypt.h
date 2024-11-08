c65a57dcba582c73f3effe2bf25736cf9d9da02c
Adriana Seda Pagan <adriana.sedapagan@mnsu.edu>
2024-11-08 13:30:50 -0600

    Added main logic, project organization and made sure it compiles

---
+++ include/encrypt.h
@@ -0,0 +1,6 @@
+#ifndef ENCRYPT_H
+#define ENCRYPT_H
+
+int encrypt_text(const char *picture_path, const char *text_path);
+
+#endif
--- src/BMPWhisper.c
+++ src/BMPWhisper.c
@@ -1,1 +1,6 @@
 #include "BMPWhisper.h"
+
+/*We will have to do fread() on each row meaning our buffer will be the horizontal resolution of the image
+  then we will have to loop through as many horizontal pixels at a maximum or until we reach the end of the
+  text they want to encrypt. We will need to deal with padding */
+
